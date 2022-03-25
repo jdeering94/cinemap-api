be09ce125f726e9612da9b4b174933958ed78402
Dani Gordon <lilyluvr723@gmail.com>
2022-03-25 13:39:19 +0100

    saved server.js lol

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import { port } from './config/environment.js';
 import router from './config/router.js';
 import { connectToDb } from './db/helpers.js';
+import mongoSanitize from 'express-mongo-sanitize';
 
 const app = express();
 app.use(express.json());
+app.use(mongoSanitize());
 app.use('/api', router);
 
 async function runServer() {
