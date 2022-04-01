f0e14a90264395d43dfaafc7bce115a9aa43a1d7
Joe Deering <JDeering94@gmail.com>
2022-04-01 16:31:16 +0100

    deploying to heroku

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { port } from './config/environment.js';
 import router from './config/router.js';
 import { connectToDb } from './db/helpers.js';
 import mongoSanitize from 'express-mongo-sanitize';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(mongoSanitize());
 app.use('/api', router);
