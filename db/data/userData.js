c8ffb1c6fb435149f2b72423b0647dc7007d95c5
Joe Deering <JDeering94@gmail.com>
2022-04-01 11:07:48 +0100

    added users

--- db/data/userData.js
+++ db/data/userData.js
@@ -3,11 +3,13 @@ const createUser = (username, email, password) => {
 };
 
 const users = [
-  createUser('user1', 'user@email.com', 'Password1!'),
+  createUser('user1', 'user1@email.com', 'Password1!'),
+  createUser('user2', 'user2@email.com', 'Password1!'),
+  createUser('user3', 'user3@email.com', 'Password1!'),
   {
     username: 'admin',
     email: 'admin@admin.com',
-    password: 'Password!1',
+    password: 'Password1!',
     isAdmin: true,
   },
 ];
